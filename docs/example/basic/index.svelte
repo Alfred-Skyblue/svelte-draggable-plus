871ddf5ca61bcebb07858aefe9795a2ad55e01a8
丶远方 <yangpanteng@gmail.com>
2023-10-24 20:24:47 +0800

    docs: update docs

--- docs/example/basic/index.svelte
+++ docs/example/basic/index.svelte
@@ -18,66 +18,16 @@
 			id: '4'
 		}
 	]
-	let list2 = [
-		{
-			name: 'Joao-2',
-			id: '1-2'
-		},
-		{
-			name: 'Jean-2',
-			id: '2-2'
-		},
-		{
-			name: 'Johanna-2',
-			id: '3-2'
-		},
-		{
-			name: 'Juan-2',
-			id: '4-2'
-		}
-	]
-
-	function clone(element) {
-		const len = list2.length
-		return {
-			name: `${element.name}-clone-${len}`,
-			id: `${element.id}-clone-${len}`
-		}
-	}
-
-	const options1 = {
-		group: { name: 'people', pull: 'clone', put: false },
-		sort: false,
-		clone
-	}
-	const options2 = {
-		group: 'people'
-	}
 </script>
 
 <div>
-	<div>
-		<Draggable
-			class="flex flex-col gap-2 p-4 w-300px h-300px m-auto bg-gray-500/5 rounded"
-			bind:items={list}
-			options={options1}
-		>
-			<div slot="element" let:item>{item.name}</div>
-		</Draggable>
-	</div>
-	<div class="mt-20 bg-blue">
-		<Draggable bind:items={list2} options={options2}>
-			<div slot="element" let:item>{item.name}</div>
-		</Draggable>
-	</div>
+	<Draggable
+		class="flex flex-col gap-2 p-4 w-300px h-300px m-auto bg-gray-500/5 rounded"
+		bind:items={list}
+		options={{ animation: 150 }}
+	>
+		<div class="cursor-move h-30 bg-gray-500/5 rounded p-3 cursor-move" slot="element" let:item>
+			{item.name}
+		</div>
+	</Draggable>
 </div>
-
-<style>
-	.mt-20 {
-		margin-top: 20px;
-	}
-
-	.bg-blue {
-		background-color: blue;
-	}
-</style>
